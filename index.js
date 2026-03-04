c7935abaec463ab5df089b30085b6c25c3bb4e33
Yasmine <yasmineharroudi2020@gmail.com>
2026-03-04 14:05:38 +0000

    test: add jsonwebtoken

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
-const express = require('express');
-const mysql = require('mysql2');
-const app = express();
+const express=require('express')
+const jwt=require('jsonwebtoken')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+/*********************************route get  */
+app.get('/api',(req,res)=>{
 
-app.use(express.json()); 
+  res.json({
 
-// Configuration de la connexion MySQL
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '0772633934yas++', 
-  database: '' 
-});
+      'msg':'Hello Fullstack'
+  })
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('Connecté à la base de données MySQL.');
-});
+})
 
-// Le serveur écoutera sur le port 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
-});
+/***********Route Post :********************/
+let secret='fullstack'
+app.post('/api/login', (req, res) => {
+          //1-recupérer les parametres :
+               // const {id,username, password } = req.body
+
+                 /*ou bien */
+
+        let id=req.body.id
+        let username=req.body.username
+        let password=req.body.password
+
+         //2- Vérifier que les informations de connexion sont valides
+        if (id==1 && username === 'yasmine' && password === '123456') {
+          const user = { id: id, username: 'cloud_natif' }
+
+          //3- Créer un jeton JWT signé
+         // const token = jwt.sign({ user }, secret, { expiresIn: '2m' })
+          const token = jwt.sign({ user }, secret)
+          res.json({ token })
+        } else {
+          res.json({ message: 'user or pass invalid !!!' })
+        }
+})
+      
+    /***********************Route créer post  */
+    app.post('/api/posts',verifytoken, (req, res) => {
+      jwt.verify(req.token,secret,(err,data)=>{
+        if(err){
+            res.sendStatus(403)          
+        }
+        
+        res.json({
+            'msg':'post created' ,
+            data   
+            })
+      })
+    })
+
+    /*********function verifytoken ***********/
+    function verifytoken(req,res,next){
+
+        // format of token  : Authorization:bearer token
+
+        const bearerHeader=req.headers['authorization']
+
+        if(typeof bearerHeader !=='undifinded')
+        {
+            // split to extract token
+            const token = bearerHeader.split(' ')[1]
+            // set token 
+            req.token=token
+            next()
+        }else{
+            res.sendStatus(403)           
+        }    
+
+    }
+
+app.listen('8000', ()=> console. log('server started on port 8000'))
